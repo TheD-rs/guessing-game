a0acf8399725073db9e72306894d29b1d644a38b
aleksei zvonarev <needforsave@gmail.com>
2023-02-27 21:25:37 +0300

    feat:add

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNum = this.min + Math.round((this.max - this.min) / 2);
+        return this.guessNum;
     }
 
     lower() {
-
+        this.max = this.guessNum;
     }
 
     greater() {
-
+        this.min = this.guessNum;
     }
 }
 
